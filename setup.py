0c4a90d454a3c822fe5c30b7806725f933475d99
Jaime McCutcheon <mccutcheon.james@gmail.com>
2022-12-01 13:44:39 +0100

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -29,8 +29,10 @@ setup(
           'matplotlib',
           'xlrd',
           'scipy',
+          'pandas',
           'xlsxwriter',
-          'tdt'
+          'tdt',
+          'trompy'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
